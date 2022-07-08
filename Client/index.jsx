3f353717e8efe9eba284a45acb381242a9f245d9
Bruce <dieselrabago@gmail.com>
2022-07-07 17:09:51 -0700

    Fixed index.jsx

--- Client/index.jsx
+++ Client/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById('app'));
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<h1>Hello World!</h1>);
