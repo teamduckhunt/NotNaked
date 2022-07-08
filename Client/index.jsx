6dc9917575c149bdc9a23eb360cdcab43c3f010a
Heemo Yang <heemo521@gmail.com>
2022-07-07 22:29:11 -0700

    Fix bug on webpack

--- Client/index.jsx
+++ Client/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-// const root = document.createElement('div');
-// root.setAttribute('id', 'root');
-// document.body.appendChild(root);
+const root = document.createElement('div');
+root.setAttribute('id', 'root');
+document.body.appendChild(root);
 
-// render(<h1>Hello World</h1>, root);
+render(<h1>Hello World</h1>, root);
