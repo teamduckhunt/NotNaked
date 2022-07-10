653710106f7312d6d7cdc1a8acae1c54b2f8b2ec
Heemo Yang <heemo521@gmail.com>
2022-07-10 13:59:24 -0700

    Add temporary comparison model that needs refatoring

--- src/index.jsx
+++ src/index.jsx
@@ -6,9 +6,18 @@ import { Provider } from 'react-redux';
 import App from './App.jsx';
 import store from './app/store';
 
+const backdrop = document.createElement('div');
+backdrop.setAttribute('id', 'backdrop');
+document.body.appendChild(backdrop);
+
+const modal = document.createElement('div');
+modal.setAttribute('id', 'modal');
+document.body.appendChild(modal);
+
 const root = document.createElement('div');
 root.setAttribute('id', 'root');
 document.body.appendChild(root);
+
 
 render(
   <Provider store={store}>
