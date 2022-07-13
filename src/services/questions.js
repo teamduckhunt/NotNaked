927fd6fcacfcf2eed956102efa6c00dd85d9ec2c
Bruce <dieselrabago@gmail.com>
2022-07-13 12:18:08 -0700

    QA RTK Mutations
    Implemented the questions api RTK mutations to add, report, and increase helpful rating for both questions/answers.

--- src/services/questions.js
+++ src/services/questions.js
@@ -4,23 +4,76 @@ import { API_KEY } from '../config/config';
 export const questionsApi = createApi({
   reducerPath: 'questionsApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/qa/questions',
+    baseUrl: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/qa',
     prepareHeaders(headers) {
       headers.set('Authorization', `${API_KEY}`);
       return headers;
     },
   }),
+  tagTypes: ['Questions', 'Answers'],
   endpoints: (builder) => ({
     allQuestions: builder.query({
-      query: (productId) => `?product_id=${productId}`,
+      query: (productId) => `/questions?product_id=${productId}`,
+      providesTags: ['Questions'],
+    }),
+    addQuestion: builder.mutation({
+      query: (productId, questionInfo) => ({
+        url: '/questions',
+        method: 'POST',
+        body: questionInfo,
+      }),
+      invalidatesTags: ['Questions'],
+    }),
+    addQuestionHelpful: builder.mutation({
+      query: (questionId) => ({
+        url: `/questions/${questionId}/helpful`,
+        method: 'PUT',
+      }),
+      invalidatesTags: ['Questions'],
+    }),
+    reportQuestion: builder.mutation({
+      query: (questionId) => ({
+        url: `/questions/${questionId}/report`,
+        method: 'PUT',
+      }),
+      invalidatesTags: ['Questions'],
     }),
     answerList: builder.query({
-      query: (questionId, page = 1, count = 5) => `/${questionId}/answers?page=${page}&count=${count}`,
+      query: (questionId, page = 1, count = 5) => `/questions/${questionId}/answers?page=${page}&count=${count}`,
+      providesTags: ['Answers'],
+    }),
+    addAnswer: builder.mutation({
+      query: (questionId, answerInfo) => ({
+        url: `/${questionId}/answers`,
+        method: 'POST',
+        body: answerInfo,
+      }),
+      invalidatesTags: ['Answers'],
+    }),
+    addAnswerHelpful: builder.mutation({
+      query: (answerId) => ({
+        url: `/${answerId}/helpful`,
+        method: 'PUT',
+      }),
+      invalidatesTags: ['Answers'],
+    }),
+    reportAnswer: builder.mutation({
+      query: (questionId) => ({
+        url: `/answers/${questionId}/report`,
+        method: 'PUT',
+      }),
+      invalidatesTags: ['Answers'],
     }),
   }),
 });
 
 export const {
   useAllQuestionsQuery,
+  useAddQuestionMutation,
+  useAddQuestionHelpfulMutation,
+  useReportQuestionMutation,
   useAnswerListQuery,
+  useAddAnswerMutation,
+  useAddAnswerHelpfulMutation,
+  useReportAnswerMutation,
 } = questionsApi;
