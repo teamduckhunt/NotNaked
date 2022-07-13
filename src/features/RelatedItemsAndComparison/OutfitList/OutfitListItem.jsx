33efb648744e408b7c3e0e3c81662a22ebc38fc2
Heemo <heemo521@gmail.com>
2022-07-13 12:34:13 -0700

    Add helpers forder in src and create getAverateRating function inside it

--- src/features/RelatedItemsAndComparison/OutfitList/OutfitListItem.jsx
+++ src/features/RelatedItemsAndComparison/OutfitList/OutfitListItem.jsx
@@ -5,24 +5,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useProductStylesQuery, useProductInformationByIdQuery } from '../../../services/products';
 import ListItemCard from '../helpers/ListItemCard/ListItemCard.jsx';
+import { useGetReviewMetadataQuery } from '../../../services/reviews';
+import getAverageRating from '../../../helpers/getAverageRating/getAverageRating';
 
 export default function OutfitListItem({ productId, handleDeleteOutfit }) {
   const { data, error, isLoading } = useProductInformationByIdQuery(productId);
-  const { data: styles } = useProductStylesQuery(productId);
+  const {data: metaData, isLoading: metaLoading} = useGetReviewMetadataQuery(productId);
+  const { data: styles, isLoading: stylesLoading } = useProductStylesQuery(productId);
   const image = styles?.results[0].photos[0].thumbnail_url || 'https://picsum.photos/200';
 
   if (error) {
     return <>Oh no, there was an error</>;
   }
 
-  if (isLoading) {
+  if (isLoading || stylesLoading || metaLoading) {
     return <>Loading...</>;
   }
+  console.log(metaData);
+  const averageRating = getAverageRating(metaData);
+
 
   return (
     <ListItemCard
       product={data}
       productId={productId}
+      averageRating={averageRating}
       productImage={image}
       actionButtonIcon="❌"
       handleOnClick={handleDeleteOutfit}
