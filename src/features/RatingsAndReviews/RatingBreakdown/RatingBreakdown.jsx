e7611d911a652788ce6a71df37c7e62738782135
Peter McBride <mrpetermcbride@gmail.com>
2022-07-14 19:29:21 -0700

    Star filter functionality in rating breakdown.
    When a star is selected the review list re-renders based on the star rating that was selected.
    Works when multiple stars are selected.

--- src/features/RatingsAndReviews/RatingBreakdown/RatingBreakdown.jsx
+++ src/features/RatingsAndReviews/RatingBreakdown/RatingBreakdown.jsx
@@ -1,13 +1,17 @@
+/* eslint-disable no-unused-expressions */
 /* eslint-disable max-len */
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useGetReviewMetadataQuery, useGetAllReviewsByProductIdQuery } from '../../../services/reviews.js';
 import Button from '../../UI/Button.jsx';
 import getAverageRating from '../../../helpers/getAverageRating/getAverageRating.js';
+import { useDispatch } from 'react-redux';
+import { setFilterByStar } from './ratingBreakdownSlice.js';
 
 export default function ReviewList({ productId }) {
   const { data, error, isLoading } = useGetReviewMetadataQuery(productId);
   const { data: reviewData, isLoading: reviewLoading } = useGetAllReviewsByProductIdQuery(productId);
+  const dispatch = useDispatch();
 
   const [starFilter, setStarFilter] = useState({
     fiveStar: false,
@@ -20,19 +24,21 @@ export default function ReviewList({ productId }) {
   const handleStarFilter = (e) => {
     console.log('event', e.target.innerText);
     if (e.target.innerText === '5 Star') {
-      setStarFilter({ starFilter, fiveStar: true });
+      starFilter.fiveStar === false ? setStarFilter({ ...starFilter, fiveStar: true }) : setStarFilter({ ...starFilter, fiveStar: false });
     }
     if (e.target.innerText === '4 Star') {
-      setStarFilter({ starFilter, fourStar: true });
+      starFilter.fourStar === false ? setStarFilter({ ...starFilter, fourStar: true })
+        : setStarFilter({ ...starFilter, fourStar: false });
     }
     if (e.target.innerText === '3 Star') {
-      setStarFilter({ starFilter, threeStar: true });
+      starFilter.threeStar === false ? setStarFilter({ ...starFilter, threeStar: true })
+        : setStarFilter({ ...starFilter, threeStar: false });
     }
     if (e.target.innerText === '2 Star') {
-      setStarFilter({ starFilter, twoStar: true });
+      starFilter.twoStar === false ? setStarFilter({ ...starFilter, twoStar: true }) : setStarFilter({ ...starFilter, twoStar: false });
     }
     if (e.target.innerText === '1 Star') {
-      setStarFilter({ starFilter, oneStar: true });
+      starFilter.oneStar === false ? setStarFilter({ ...starFilter, oneStar: true }) : setStarFilter({ ...starFilter, oneStar: false });
     }
   };
 
@@ -45,8 +51,8 @@ export default function ReviewList({ productId }) {
   }
 
   if (data || reviewData) {
-    console.log('data', data);
-    console.log('reviewData', reviewData);
+    // console.log('data', data);
+    // console.log('reviewData', reviewData);
     const avgRating = getAverageRating(data);
     const numOfReviews = reviewData.results.length;
     return (
@@ -58,21 +64,48 @@ export default function ReviewList({ productId }) {
             ⭐️
           </div>
           {numOfReviews}
+          {' '}
+          total reviews
         </div>
         <div>
-          <Button onClick={(e) => { handleStarFilter(e); }}>
+          <Button
+            onClick={(e) => {
+              handleStarFilter(e);
+              dispatch(setFilterByStar(5));
+            }}
+          >
             5 Star
           </Button>
-          <Button onClick={(e) => { handleStarFilter(e); }}>
+          <Button
+            onClick={(e) => {
+              handleStarFilter(e);
+              dispatch(setFilterByStar(4));
+            }}
+          >
             4 Star
           </Button>
-          <Button onClick={(e) => { handleStarFilter(e); }}>
+          <Button
+            onClick={(e) => {
+              handleStarFilter(e);
+              dispatch(setFilterByStar(3));
+            }}
+          >
             3 Star
           </Button>
-          <Button onClick={(e) => { handleStarFilter(e); }}>
+          <Button
+            onClick={(e) => {
+              handleStarFilter(e);
+              dispatch(setFilterByStar(2));
+            }}
+          >
             2 Star
           </Button>
-          <Button onClick={(e) => { handleStarFilter(e); }}>
+          <Button
+            onClick={(e) => {
+              handleStarFilter(e);
+              dispatch(setFilterByStar(1));
+            }}
+          >
             1 Star
           </Button>
         </div>
@@ -88,11 +121,11 @@ ReviewList.propTypes = {
 
 // Strategy
 
-// product rating from helpers/getAverageRating
+// product rating from helpers/getAverageRating DONE
 // iterate over the results of current product, grab the ratings and push to a separate array.
-// so maybe map that return an array, pass that array of ratings to getAverageRating. (return to one decimal point)
+// so maybe map that return an array, pass that array of ratings to getAverageRating. (return to one decimal point) DONE
 
-// number of reviews based on product id
+// number of reviews based on product id DONE
 
 // create div block, with 5 Star Buttons, need to add some toggle on : off event listener
 // when toggled on, the review list should re-render to only the indicated star.
---
+++ src/features/RatingsAndReviews/RatingBreakdown/ratingBreakdownSlice.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-param-reassign */
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  filterByStar: [],
+};
+
+export const RatingBreakdownSlice = createSlice({
+  name: 'rating',
+  initialState,
+  reducers: {
+    setFilterByStar: (state, action) => {
+      if (!state.filterByStar.includes(action.payload)) {
+        state.filterByStar.push(action.payload);
+      } else {
+        const location = state.filterByStar.indexOf(action.payload);
+        state.filterByStar.splice(location, 1);
+      }
+    },
+  },
+});
+
+export const { setFilterByStar } = RatingBreakdownSlice.actions;
+export default RatingBreakdownSlice.reducer;
+
+// create a new method to reset filterByStar to just an empty array.
