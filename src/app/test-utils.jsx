fc20f4ad0267348c44b28d87676f5b278d6e5ae9
Heemo Yang <heemo521@gmail.com>
2022-07-18 14:38:55 -0700

    Update test-utils to hold all reducers

--- src/app/test-utils.jsx
+++ src/app/test-utils.jsx
@@ -8,13 +8,14 @@ import {
 import { render as rtlRender } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
-import { productsApi } from '../services/products.js';
-import { reviewsApi } from '../services/reviews.js';
-import { questionsApi } from '../services/questions.js';
 import outfitListReducer from '../features/RelatedItemsAndComparison/OutfitList/outfitListSlice.jsx';
 import questionsAndAnswersReducer from '../features/QuestionsAndAnswers/questionsAndAnswersSlice.jsx';
 import ratingBreakdownReducer from '../features/RatingsAndReviews/RatingBreakdown/ratingBreakdownSlice.js';
 import sortItemsReducer from '../features/RatingsAndReviews/SortItems/sortItemsSlice.js';
+import { productsApi } from '../services/products.js';
+import { reviewsApi } from '../services/reviews.js';
+import { questionsApi } from '../services/questions.js';
+import productStylesReducer from '../features/Overview/StyleSelector/styleSlice.js';
 
 function reducer(
   ui,
@@ -23,9 +24,13 @@ function reducer(
     store = configureStore({
       reducer: {
         [productsApi.reducerPath]: productsApi.reducer,
-        [reviewsApi.reducerPath]: reviewsApi.reducer,
         outfitList: outfitListReducer,
+        [reviewsApi.reducerPath]: reviewsApi.reducer,
         [questionsApi.reducerPath]: questionsApi.reducer,
+        questionsAndAnswers: questionsAndAnswersReducer,
+        productStyles: productStylesReducer,
+        ratingBreakdown: ratingBreakdownReducer,
+        sortItems: sortItemsReducer,
       },
       preloadedState,
       middleware: (getDefaultMiddleware) => getDefaultMiddleware()
