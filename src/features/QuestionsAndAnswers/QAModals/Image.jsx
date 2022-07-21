614e3ec26fade01d9f4c52539c34c14aa6b6b4ad
Bruce <dieselrabago@gmail.com>
2022-07-20 19:20:59 -0700

    Slight refactor

--- src/features/QuestionsAndAnswers/QAModals/Image.jsx
+++ src/features/QuestionsAndAnswers/QAModals/Image.jsx
@@ -8,7 +8,7 @@ import styles from './AddQA.module.css';
 
 export default function Image({ image, handleModalToggle }) {
   return (
-    <Modal className={styles.modal}>
+    <Modal className={styles.modalIMG}>
       <img className={styles.image} src={image} alt="answer" />
       <Button onClick={() => handleModalToggle()} className={styles.imageClose}>
         Close
