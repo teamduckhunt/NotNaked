675d62a977295be011063c6a23eb1b73c968500a
Heemo <heemo521@gmail.com>
2022-07-22 02:43:53 -0700

    Add compression

--- server/index.js
+++ server/index.js
@@ -4,6 +4,11 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+
+const compression = require('compression');
+
+app.use(compression());
+
 app.use(express.static(path.join(__dirname, '..', 'dist')));
 
 app.get('*', (req, res) => {
